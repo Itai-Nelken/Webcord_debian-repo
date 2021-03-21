806409e9638fe15f9c8d8d64debac738c82ec35f
Itai Nelken <70802936+Itai-Nelken@users.noreply.github.com>
2021-03-21 11:11:12 +0200

    Update add-repo.sh

--- add-repo.sh
+++ add-repo.sh
@@ -2,6 +2,13 @@
 
 function error() {
   echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
+  exit 1
+}
+
+function warning() {
+  echo -e "$(tput setaf 1)$(tput bold)$1$(tput sgr 0)"
+  sleep 2
+  true
 }
 
 #check that script is being run as root
@@ -13,13 +20,13 @@ fi
 
 echo -e "$(tput setaf 6)adding repo...$(tput sgr 0)"
 echo ' + sudo wget https://raw.githubusercontent.com/Itai-Nelken/electron-discord-webapp_debian-repo/main/discord-webapp.list -O /etc/apt/sources.list.d/discord-webapp.list'
-sudo wget https://raw.githubusercontent.com/Itai-Nelken/electron-discord-webapp_debian-repo/main/discord-webapp.list -O /etc/apt/sources.list.d/discord-webapp.list || error "Failed to download 'discord-webapp.list'!"; exit 1
+sudo wget https://raw.githubusercontent.com/Itai-Nelken/electron-discord-webapp_debian-repo/main/discord-webapp.list -O /etc/apt/sources.list.d/discord-webapp.list || error "Failed to download 'discord-webapp.list'!"
 echo "adding key..."
 echo ' + wget -qO- https://raw.githubusercontent.com/Itai-Nelken/electron-discord-webapp_debian-repo/main/KEY.gpg | sudo apt-key add -'
-wget -qO- https://raw.githubusercontent.com/Itai-Nelken/electron-discord-webapp_debian-repo/main/KEY.gpg | sudo apt-key add - || error "Failed to download and add key!"; exit 1
+wget -qO- https://raw.githubusercontent.com/Itai-Nelken/electron-discord-webapp_debian-repo/main/KEY.gpg | sudo apt-key add - || error "Failed to download and add key!"
 echo -e "$(tput setaf 6)running apt update...$(tput sgr 0)"
 echo " + sudo apt update"
-sudo apt update || error "Failed to run 'sudo apt update'! please run that"; true; sleep 3
+sudo apt update || warning "Failed to run 'sudo apt update'! please run that"
 echo -e "$(tput setaf 2)DONE!$(tput sgr 0)"
 echo -e "$(tput setaf 6)$(tput bold)To install electron-discord-webapp, run:$(tput sgr 0)"
 echo -e "$(tput bold)sudo apt install electron-discord-webapp$(tput sgr 0)"
